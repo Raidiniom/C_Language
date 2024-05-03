7cd5008a432e85fcec112aac0db0a4070aeb6586
Raidiniom <raidiniom22PH@gmail.com>
2024-05-04 01:41:16 +0800

    wa

---
+++ Practice/roro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct 
+{
+    /* data */
+} ;
+
+typedef struct 
+{
+    /* data */
+} ;
+
+typedef struct 
+{
+    /* data */
+} ;
+
+
+int main()
+{
+
+    return 0;
+}
