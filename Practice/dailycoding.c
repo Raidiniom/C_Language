0c0d427d58805ea3a0793594f131d59bac581df6
Raidiniom <raidiniom22ph@gmail.com>
2024-07-26 18:29:05 +0800

    ga testing rako ani

---
+++ Practice/dailycoding.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef char String[6];
+
+int main () {
+    // int n, totl = 0;
+
+    // printf("Enter positive number: ");
+    // scanf("%d", &n);
+
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+    //     {
+    //         totl += i; 
+    //     }
+        
+    // }
+    
+    // printf("Total: %d", totl);
+
+
+    return 0;
+}
