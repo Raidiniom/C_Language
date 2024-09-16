e84f9daf08f15d43a6232435d7fc5cd31a6a383a
Raidiniom <132919460+Raidiniom@users.noreply.github.com>
2024-09-16 18:46:41 +0000

    learn sorting this shiet man

--- Practice/DSA/pracmain1.c
+++ Practice/DSA/pracmain1.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "pracst.h"
 
 int main() {
+    Stack mystack;
+
+    initStack(&mystack);
+
+    Person p1 = createPerson("Voltaire", "Rati", 21, 'm');
+    Person p2 = createPerson("Charles", "Boqe", 20, 'm');
+    Person p3 = createPerson("Summi", "Dera", 19, 'f');
+    Person p4 = createPerson("Alyssa", "Jo", 19, 'f');
+    Person p5 = createPerson("Chelsea", "Colla", 21, 'f');
+    Person p6 = createPerson("Rence", "Aber", 20, 'm');
+
+    push(&mystack, p2);
+    push(&mystack, p6);
+    push(&mystack, p1);
+    push(&mystack, p3);
+    push(&mystack, p4);
+    push(&mystack, p5);
+
+    displayStack(mystack);
 
     return 0;
 }
--- Practice/DSA/pracst.h
+++ Practice/DSA/pracst.h
@@ -1,10 +1,16 @@
 #ifndef PRACST_H
 #define PRACST_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+
 typedef char String[30];
 
 typedef struct {
-    String name;
+    String fname;
+    String lname;
     int age;
     char sex;
 } Person;
@@ -14,13 +20,64 @@ typedef struct node{
     struct node *link;
 } NodeType, *NodePtr, *Stack;
 
-void initStack(Stack *s);
-bool isEmpty(Stack s);
-void displayStack(Stack s);
+Person createPerson(String fname, String lname, int age, char sex) {
+  Person start;
 
-bool push(Stack *s, Person p);
-bool pop(Stack *s);
-Person peek(Stack s);
+  strcpy(start.fname, fname);
+  strcpy(start.lname, lname);
+  start.age = age;
+  start.sex = sex;
+
+  return start;
+}
+
+void initStack(Stack *s) {
+  *s =(NodePtr) malloc(sizeof(NodeType));
+
+  (*s)->link = NULL;
+
+}
+bool isEmpty(Stack s) {
+  return s->link == NULL;
+}
+void displayStack(Stack s) {
+  if (!isEmpty(s))
+  {
+    printf("Person List:\n");
+    while (s->link != NULL)
+    {
+      printf("%.30s, %.30s %d %c\n", s->data.fname, s->data.lname, s->data.age, s->data.sex);
+      s = s->link;
+    }
+    printf("\n");
+    
+  }
+  
+}
+
+bool push(Stack *s, Person p) {
+  Stack start =(NodePtr) malloc(sizeof(NodeType));
+
+  start->data = p;
+  start->link = *s;
+  *s = start;
+  
+}
+bool pop(Stack *s) {
+  if (!isEmpty(*s))
+  {
+    Stack hold;
+
+    hold = *s;
+    *s = (*s)->link;
+
+    free(hold);
+  }
+  
+}
+Person peek(Stack s) {
+  return s->data;
+}
 
 /*Insert sorted base on lastname. Rember to use the property 
   of the stack using push, pop, and peek functions.*/
